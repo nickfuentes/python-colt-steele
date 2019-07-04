464e78bbfec13147dad0179890b6134f8e8ba2fc
Full Name <nickfuentes24@gmail.com>
2019-07-04 12:16:00 -0500

    Refactores the locgical age proccess to look cleaner.

--- python-numbers-operators-comments/bouncer.py
+++ python-numbers-operators-comments/bouncer.py
@@ -4,11 +4,11 @@ user_age = input("How old are you? ")
 
 if user_age != "":
     # 18-21 wristband
-    if int(user_age) >= 18 and int(user_age) < 21:
-        print("You need a wristband to enter.")
-    # 21+ drink, normal entry
-    elif int(user_age) >= 21:
+    if int(user_age) >= 21:
         print("You are older than 21 go ahead!")
+    # 21+ drink, normal entry
+    elif int(user_age) >= 18:
+        print("You need a wristband to enter!")
     # too young, sorry
     else:
         print("Sorry you are too young you can not enter.")
