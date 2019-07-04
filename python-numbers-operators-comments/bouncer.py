cb5df5085f1828bc383ee5f7f3fcffd608410891
Full Name <nickfuentes24@gmail.com>
2019-07-04 12:06:35 -0500

    Wrote the code so that if the user does not enter anything it exits. But also a way to allow for not just ints to be in but for strings to build on top for other apps.

--- python-numbers-operators-comments/bouncer.py
+++ python-numbers-operators-comments/bouncer.py
@@ -1,12 +1,15 @@
 # ask for age
-user_age = int(input("How old are you? "))
+user_age = input("How old are you? ")
 
-# 18-21 wristband
-if user_age >= 18 and user_age < 21:
-    print("You need a wristband to enter.")
-# 21+ drink, normal entry
-elif user_age >= 21:
-    print("You are older than 21 go ahead!")
-# too young, sorry
+if user_age != "":
+    # 18-21 wristband
+    if int(user_age) >= 18 and int(user_age) < 21:
+        print("You need a wristband to enter.")
+    # 21+ drink, normal entry
+    elif int(user_age) >= 21:
+        print("You are older than 21 go ahead!")
+    # too young, sorry
+    else:
+        print("Sorry you are too young you can not enter.")
 else:
-    print("Sorry you are too young you can not enter.")
+    print("You need to enter an age!")
