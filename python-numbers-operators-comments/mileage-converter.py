7265d9746d0fe0f0be517d4edbdbda2fe08070b0
Full Name <nickfuentes24@gmail.com>
2019-07-04 10:43:23 -0500

    Changed message that is printed to user.

--- python-numbers-operators-comments/mileage-converter.py
+++ python-numbers-operators-comments/mileage-converter.py
@@ -2,10 +2,11 @@ user_input = float(input("How many kilometers did you run today? "))
 
 
 def convert_kilo_to_miles():
-    miles = user_input / 1.60934
+    miles = round(user_input / 1.60934, 2)
     return miles
 
 
 miles = convert_kilo_to_miles()
 
-print(f"Okay you said you ran {user_input} the mile you ran is {miles}")
+print(
+    f"Okay you said you ran {user_input}km. That is {miles} miles! Great Job!")
