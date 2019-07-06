270de16224920e2746da5a0409b60ebbb69085c0
Full Name <nickfuentes24@gmail.com>
2019-07-05 22:05:26 -0500

    Put the game in the loop to let the player continue playing and added a score box and limit to end game.

--- python-numbers-operators-comments/ai-paper-rock-scissors-game.py
+++ python-numbers-operators-comments/ai-paper-rock-scissors-game.py
@@ -16,25 +16,46 @@ elif rand_num == 2:
 elif rand_num == 3:
     comp_choice = "paper"
 
-print_game_title()
-player = input("Player choose one [paper] [rock] [scissors] ").lower()
+player_wins = 0
+computer_wins = 0
+winning_score = 4
 
-if player == comp_choice:
-    print(f"It Is A Draw! Computer chose {comp_choice}")
-elif player == "rock":
-    if comp_choice == "scissors":
-        print(f"Player One Wins! Computer chose {comp_choice}")
-    elif comp_choice == "paper":
-        print(f"Computer Wins! Computer chose {comp_choice}")
-elif player == "paper":
-    if comp_choice == "rock":
-        print(f"Player One Wins! Computer chose {comp_choice}")
-    elif comp_choice == "scissors":
-        print(f"Computer Wins! Computer chose {comp_choice}")
-elif player == "scissors":
-    if comp_choice == "paper":
-        print(f"Player One Wins! Computer chose {comp_choice}")
-    elif comp_choice == "rock":
-        print(f"Computer Wins! Computer chose {comp_choice}")
+while player_wins < winning_score and computer_wins < winning_score:
+    print_game_title()
+    player = input("Player choose one [paper] [rock] [scissors] ").lower()
+
+    if player == comp_choice:
+        print(f"It Is A Draw! Computer chose {comp_choice}!")
+    elif player == "rock":
+        if comp_choice == "scissors":
+            print(f"Player One Wins! Computer chose {comp_choice}!")
+            player_wins += 1
+        elif comp_choice == "paper":
+            print(f"Computer Wins! Computer chose {comp_choice}!")
+            computer_wins += 1
+    elif player == "paper":
+        if comp_choice == "rock":
+            print(f"Player One Wins! Computer chose {comp_choice}!")
+            player_wins += 1
+        elif comp_choice == "scissors":
+            print(f"Computer Wins! Computer chose {comp_choice}!")
+            computer_wins += 1
+    elif player == "scissors":
+        if comp_choice == "paper":
+            print(f"Player One Wins! Computer chose {comp_choice}!")
+            player_wins += 1
+        elif comp_choice == "rock":
+            print(f"Computer Wins! Computer chose {comp_choice}!")
+            computer_wins += 1
+    else:
+        print("Something Went Wrong!")
+    print(f"Score: player: {player_wins} computer: {computer_wins}")
+
+if player_wins > computer_wins:
+    print("----------------------------------")
+    print("YOU WIN!!!")
 else:
-    print("Something Went Wrong!")
+    print("----------------------------------")
+    print("YOU LOSE!")
+print(f"FINAL SCORE: player: {player_wins} computer: {computer_wins}")
+print("----------------------------------")
