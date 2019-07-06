53ee75fbb2df75df99c118677f67b6ae8e817752
Full Name <nickfuentes24@gmail.com>
2019-07-05 22:09:17 -0500

    * art triangle with loops in python.

---
+++ python-loops/art-tree.py
@@ -0,0 +1,11 @@
+
+
+while True:
+    command = input("Type 'exit' to exit: ")
+    if (command == "exit"):
+        break
+
+for index in range(1, 101):
+    print(index)
+    if index == 3:
+        break
